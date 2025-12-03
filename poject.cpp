459495185bfb37c22341c480ca7c434bfc2c163f
Fady-tamer <fadytamer2018@gmail.com>
2025-12-03 22:39:50 +0200

    add compare expressions function

--- poject.cpp
+++ poject.cpp
@@ -65,6 +65,65 @@ void solveExpression(int var_count, const string expression_name, const function
         cout << "| " << expression_name << " IS NOT a TAUTOLOGY.\n";
     cout << "=============================================\n\n";
 }
+void compareExpressions(int var_count, const string LHS, const string RHS, const function<bool(bool, bool, bool)> expression, const function<bool(bool, bool, bool)> other_expression)
+{
+    int num_rows = pow(2, var_count);
+    cout << "=============================================\n";
+    cout << "| Expression: " << LHS << " (L.H.S) |" << "\n";
+    cout << "=============================================\n";
+    cout << "| A | B | " << LHS << " |\n";
+    cout << "=============================================\n";
+
+    bool is_equivalence = true;
+    for (int i = num_rows - 1; i >= 0; i--)
+    {
+
+        bool A = (var_count == 2) ? (i / 2) % 2 == 1 : (i / 4) % 2 == 1;
+        bool B = (var_count == 2) ? i % 2 == 1 : (i / 2) % 2 == 1;
+        bool C = (var_count == 2) ? unsigned{0} : i % 2 == 1;
+
+        bool LHS = (var_count == 2) ? expression(A, B, C) : expression(A, B, C);
+        bool RHS = (var_count == 2) ? other_expression(A, B, C) : other_expression(A, B, C);
+        if (LHS != RHS)
+        {
+            is_equivalence = false;
+        }
+        cout << "| " << print_bool(A);
+        cout << " | " << print_bool(B);
+        cout << " |   " << print_bool(LHS) << endl;
+    }
+
+    cout << "=============================================\n";
+    cout << "| Expression: " << RHS << " (R.H.S) |" << "\n";
+    cout << "=============================================\n";
+    cout << "| A | B | " << RHS << " |\n";
+    cout << "=============================================\n";
+
+    for (int i = num_rows - 1; i >= 0; i--)
+    {
+
+        bool A = (var_count == 2) ? (i / 2) % 2 == 1 : (i / 4) % 2 == 1;
+        bool B = (var_count == 2) ? i % 2 == 1 : (i / 2) % 2 == 1;
+        bool C = (var_count == 2) ? unsigned{0} : i % 2 == 1;
+
+        bool LHS = (var_count == 2) ? expression(A, B, C) : expression(A, B, C);
+        bool RHS = (var_count == 2) ? other_expression(A, B, C) : other_expression(A, B, C);
+        if (LHS != RHS)
+        {
+            is_equivalence = false;
+        }
+        cout << "| " << print_bool(A);
+        cout << " | " << print_bool(B);
+        cout << " |   " << print_bool(RHS) << endl;
+    }
+
+    cout << "=============================================\n";
+    if (is_equivalence)
+        cout << "| " << LHS << " and " << RHS << " is equivalence.\n";
+    else
+        cout << "| " << LHS << " and " << RHS << " is't equivalence.\n";
+    cout << "=============================================\n\n";
+}
 
 void solve()
 {
@@ -78,6 +137,22 @@ void solve()
         "A -> (B -> A)",
         [](bool A, bool B, unsigned)
         { return Implication(A, Implication(B, A)); });
+    compareExpressions(
+        2,
+        "A ^ B",
+        "B ^ A",
+        [](bool A, bool B, unsigned)
+        { return Conjunction(A, B); },
+        [](bool A, bool B, unsigned)
+        { return Conjunction(B, A); });
+    compareExpressions(
+        2,
+        "A ^ B",
+        "B -> A",
+        [](bool A, bool B, unsigned)
+        { return Conjunction(A, B); },
+        [](bool A, bool B, unsigned)
+        { return Implication(B, A); });
 }
 
 signed main()
